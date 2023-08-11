7411ad228cd934cf1a96d9d8802325fca9585e02
AroNixBox <nixonbusinessmail@gmail.com>
2023-08-11 15:25:58 +0200

    Added Project FIles

---
+++ FreseJam/Source/FreseJam/FreseJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FreseJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFreseJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFreseJamGameMode();
+};
+
+
+
